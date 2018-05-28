836efb7c2e6d3ebc327643401c06a3e7d8db90b5
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2018-05-28 16:53:09 +0200

    Further simplified code (from yet unused features) and rebuild 0.0.29.

--- src/main/groovy/de/dkfz/roddy/bam2fastq/BamToFastqPlugin.java
+++ src/main/groovy/de/dkfz/roddy/bam2fastq/BamToFastqPlugin.java
@@ -9,8 +9,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
 
 class BamToFastqPlugin extends BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "0.0.28";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Mon May 28 16:50:09 CEST 2018";
+    public static final String CURRENT_VERSION_STRING = "0.0.29";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Mon May 28 16:52:48 CEST 2018";
 
     @Override
     public String getVersionInfo() {
--- src/main/groovy/de/dkfz/roddy/bam2fastq/BamToFastqWorkflow.groovy
+++ src/main/groovy/de/dkfz/roddy/bam2fastq/BamToFastqWorkflow.groovy
@@ -108,11 +108,9 @@ class BamToFastqWorkflow extends Workflow {
      * @param fastq2
      * @param fastq3
      */
-    void sortFastqs(String bamFileName, String readGroup, BaseFile fastq1, BaseFile fastq2 = null, BaseFile fastq3 = null) {
+    void sortFastqs(String bamFileName, String readGroup, BaseFile fastq1, BaseFile fastq2 = null) {
         assert(fastq1 != null)
         assert(fastq1.absolutePath != fastq2?.absolutePath)
-        assert(fastq1.absolutePath != fastq3?.absolutePath)
-        assert(fastq2 == null || fastq2.absolutePath != fastq3?.absolutePath)
         HashMap<String, String> parameters = [readGroup: readGroup, bamFileName: bamFileName]
         if (config.pairedEnd) {
             if (!config.writeUnpairedFastq) {
