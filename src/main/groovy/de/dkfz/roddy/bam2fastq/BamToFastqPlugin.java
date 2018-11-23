619478b1883a09a0cf892c07b4fa66ee51973182
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2018-11-23 15:49:38 +0100

    Compile 1.1.0.

--- src/main/groovy/de/dkfz/roddy/bam2fastq/BamToFastqPlugin.java
+++ src/main/groovy/de/dkfz/roddy/bam2fastq/BamToFastqPlugin.java
@@ -10,7 +10,7 @@ import de.dkfz.roddy.plugins.BasePlugin;
 class BamToFastqPlugin extends BasePlugin {
 
     public static final String CURRENT_VERSION_STRING = "1.1.0";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Mon Sep 17 13:00:36 CEST 2018";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Fri Nov 23 15:49:23 CET 2018";
 
     @Override
     public String getVersionInfo() {
